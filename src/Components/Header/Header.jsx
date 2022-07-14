cab10c7c8efc6628f2dcdcce70e832cd80d46106
amvlasenko <syringe.prod@gmail.com>
2022-07-14 22:54:21 +0300

    Release v1.2 (fix routing)

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,9 +7,9 @@ function Header() {
       <header className="Header">
          <nav>
             <div className="nav-wrapper #5c6bc0 indigo lighten-1">
-               <Link to="ab-spa/home" className="brand-logo right">ab-spa</Link>
+               <Link to="ab-spa" className="brand-logo right">ab-spa</Link>
                <ul id="nav-mobile" className="left">
-                  <li><Link to="ab-spa/home">Home</Link></li>
+                  <li><Link to="ab-spa">Home</Link></li>
                   <li><Link to="ab-spa/about">About</Link></li>
                </ul>
             </div>
--- src/app/router.js
+++ src/app/router.js
@@ -3,7 +3,7 @@ import About from '../Components/About/About';
 import NotFound from '../Components/NotFound/NotFound';
 
 export const userRoutes = [
-   {path: 'ab-spa/home', component: <Home/>},
+   {path: 'ab-spa', component: <Home/>},
    {path: 'ab-spa/about', component: <About/>},
    {path: '*', component: <NotFound/>},
 ];
